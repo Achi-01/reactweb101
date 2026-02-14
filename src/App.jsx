5743f02bdd0721a39659fcc604fb91e317f11fc6
Achita <Achicup2007@gmail.com>
2026-02-14 16:56:53 +0700

    Newcode

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,28 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className= 'mt-14 mx auto max-w-6xl grid grid-cols-[40%_60%]'>
-    <div>
-    <div className='sticky top-0 grid grid-rows-[35%_40%_20%]'> 
+    <div className='mt-14 mx-auto max-w-6xl grid-cols-[40%_60%]'>
       <div>
-        <div>Achita Kasamepotnthaweekij</div>
-      <div>Com-Sci</div>
-      <div>Lasy but Very</div>
-      <div>View Resume</div>
-      <div/>
-      <div>Nav</div>
-      <div className='flex items-end'>Link</div>
+      <div className='sticky top-0 grid-rows-[35%_40%_20%]'>
+        <div>
+        <div>Name</div>
+        <div>Title</div>
+        <div>Caption</div>
+        <div>View Resume</div>
+        </div>
+        <div>Nav</div>
+        <div className='flex items-end'>Link</div>
+      </div>
+      </div>
+      <div>
+        <div>About</div>
+        <div className = 'mb-80'>Experience</div>
+        <div className = 'mb-80'>Experience</div>
+        <div className = 'mb-80'>Experience</div>
+        <div className = 'mb-80'>Experience</div>
+        <div className = 'mb-80'>Experience</div>
+      </div>
     </div>
-    </div>
-    <div>About</div>
-    <div className='mb-40'>Experience</div>
-    <div className='mb-40'>Experience</div>
-    <div className='mb-40'>Experience</div>
-    <div className='mb-40'>Experience</div>
-    <div className='mb-40'>Experience</div>
-    <div className='mb-40'>Experience</div>
-    </div>
-  </div>
   )
 }
 export default App
