2076ec7e4f91df7c71daef13eb89603c50529c40
Achita <Achicup2007@gmail.com>
2026-02-14 21:04:10 +0700

    newcode

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='mt-14 mx-auto max-w-6xl grid-cols-[40%_60%]'>
+    
+    <div className='mt-14 mx-auto max-w-6xl grid grid-cols-[40%_60%]'>
       <div>
-      <div className='sticky top-0 grid-rows-[35%_40%_20%]'>
+        <div className='sticky top-0 grid grid-rows-[35%_45%_20%]'>
         <div>
         <div>Name</div>
         <div>Title</div>
@@ -15,17 +16,19 @@ function App() {
         <div>View Resume</div>
         </div>
         <div>Nav</div>
-        <div className='flex items-end'>Link</div>
+        <div className='fliex items-end'>Link</div>
       </div>
       </div>
-      <div>
+       <div>
         <div>About</div>
-        <div className = 'mb-80'>Experience</div>
-        <div className = 'mb-80'>Experience</div>
-        <div className = 'mb-80'>Experience</div>
-        <div className = 'mb-80'>Experience</div>
-        <div className = 'mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
       </div>
+    
     </div>
   )
 }
